c5a9c7057d4bce8b9f53b91b9c9f4f8db11b55eb
Peggy535 <peggy161152@gmail.com>
2022-09-23 12:15:04 +0100

    twicpics added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@ import { defineNuxtConfig } from "nuxt";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  modules: ["@nuxtjs/tailwindcss"],
+  modules: ["@nuxtjs/tailwindcss", "@twicpics/components/nuxt3"],
   css: ["~/assets/css/tailwind.css"],
   postcss: {
     plugins: {
@@ -12,5 +12,8 @@ export default defineNuxtConfig({
   },
   build: {
     transpile: ["gsap"],
+  },
+  twicpics: {
+    domain: "https://sugar-loaf-images.twic.pics/",
   },
 });
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <script setup></script>
 <template>
-  <div class="overscroll-none divide-y-2 divide-black">
+  <div class="h-auto w-screen overflow-hidden divide-y-2 divide-black">
     <!-- Video section -->
     <section class="h-screen w-screen fixed">
       <video
@@ -20,7 +20,7 @@
       </video>
     </section>
     <!-- Landing title -->
-    <section class="relative h-screen w-screenz-20 bg-white bg-opacity-5 z-20">
+    <section class="relative h-screen w-full bg-amber-500 bg-opacity-20 z-20">
       <div class="flex flex-col absolute bottom-0 left-0 m-1 p-1 mb-20">
         <h1
           class="text-6xl sm:text-9xl lg:text-11xl 2xl:text-13xl text-white mb-10"
@@ -35,6 +35,17 @@
         Please scroll down Worthers!
       </aside>
     </section>
-   
+    <section
+      class="relative h-fit w-full bg-amber-50 grid-cols-1 grid-rows-1 z-20"
+    >
+      <div class="grid col-span-1 row-span-1 md:col-span-2 md:row-span-1">
+        <div class="flex flex-col flex-shrink">
+          <p class="text-4xl m-3 p-3 border-l-2 border-l-black">
+            "Set in large and quiet gardens overlooking Falmouth Harbour, Sugar
+            Loaf has views to swoon at and plenty of privacy, peace and quiet."
+          </p>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
