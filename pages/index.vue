638080912dee441582aed7980af398d873babc6f
Peggy535 <peggy161152@gmail.com>
2022-09-23 15:00:41 +0100

    updates to landing page

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <script setup></script>
 <template>
-  <div class="h-auto w-screen overflow-hidden divide-y-2 divide-black">
+  <div class="h-auto w-screen overflow-hidden">
     <!-- Video section -->
     <section class="h-screen w-screen fixed">
       <video
@@ -36,14 +36,69 @@
       </aside>
     </section>
     <section
-      class="relative h-fit w-full bg-amber-50 grid-cols-1 grid-rows-1 z-20"
+      class="relative max-h-fit w-screen bg-amber-50 grid grid-cols-1 grid-rows-1 z-20"
     >
       <div class="grid col-span-1 row-span-1 md:col-span-2 md:row-span-1">
-        <div class="flex flex-col flex-shrink">
-          <p class="text-4xl m-3 p-3 border-l-2 border-l-black">
+        <div class="flex flex-col w-full items-center">
+          <p
+            class="text-4xl md:text-6xl m-3 p-3 my-10 border-l-2 border-l-black"
+          >
             "Set in large and quiet gardens overlooking Falmouth Harbour, Sugar
             Loaf has views to swoon at and plenty of privacy, peace and quiet."
           </p>
+          <div class="m-2 p-2 sm:m-5 sm:p-5 md:h-2/3 items-center">
+            <cite class="text-xs"
+              >The front court yard and entrance to Sugar Loaf</cite
+            >
+            <img
+              class="object-contain rounded-2xl py-2"
+              src="https://res.cloudinary.com/peggy-co/image/upload/f_auto,q_auto:best/v1663840537/SugarLoaf/CourtYard_khmzbz.jpg"
+              alt=""
+            />
+            <img
+              class="object-contain rounded-2xl"
+              src="https://res.cloudinary.com/peggy-co/image/upload/f_auto,q_auto:best/v1663840542/SugarLoaf/PoolViewOfHarbour_qqmxlg.jpg"
+              alt=""
+            />
+            <cite class="text-xs"
+              >Views of Falmouth Harbour from the private pool</cite
+            >
+          </div>
+        </div>
+      </div>
+    </section>
+    <section
+      class="relative max-h-fit w-screen bg-amber-50 grid grid-cols-1 grid-rows-1 z-20"
+    >
+      <div class="grid col-span-1 row-span-1 md:col-span-2 md:row-span-1">
+        <div class="flex flex-col w-full items-center">
+          <p
+            class="text-4xl md:text-6xl m-3 p-3 my-10 border-l-2 border-l-black"
+          >
+            "Situated 5 minutes drive from the beaches and restaurants of
+            English Harbour, the property has four large double bedrooms all
+            with A/C; a big kitchen and sitting room and a gallery that really
+            is just perfect - both day and night, for boat watching or just
+            gazing at the Sea."
+          </p>
+          <div class="m-2 p-2 sm:m-5 sm:p-5 md:h-2/3 items-center">
+            <cite class="text-xs"
+              >Sit back, relax, chat and ponder over the awe inspiring view</cite
+            >
+            <img
+              class="object-contain rounded-2xl py-2"
+              src="https://res.cloudinary.com/peggy-co/image/upload/f_auto,q_auto:best/v1663840543/SugarLoaf/veranda_ipjnac.jpg"
+              alt=""
+            />
+            <img
+              class="object-contain rounded-2xl"
+              src="https://res.cloudinary.com/peggy-co/image/upload/f_auto,q_auto:best/v1663840540/SugarLoaf/MainLivingRoom3_njhdjo.jpg"
+              alt=""
+            />
+            <cite class="text-xs"
+              >Sugar Loafs living area</cite
+            >
+          </div>
         </div>
       </div>
     </section>
