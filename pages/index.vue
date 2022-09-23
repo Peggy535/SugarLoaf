e5aed91725d5ea9a9f0d2aab3b80f5a27aa818be
Peggy535 <peggy161152@gmail.com>
2022-09-23 18:22:39 +0100

    menu options on landing page

--- pages/index.vue
+++ pages/index.vue
@@ -5,9 +5,11 @@
     <section class="h-screen w-screen fixed">
       <video
         class="absolute top-0 right-0 w-screen h-screen object-center object-cover"
+        src="https://res.cloudinary.com/peggy-co/video/upload/f_auto,q_auto:best/v1663840583/SugarLoaf/antigua_sea_qof8pg.mp4"
         autoplay
         loop
         muted
+        playsinline
       >
         <source
           src="https://res.cloudinary.com/peggy-co/video/upload/q_auto:best/v1663840583/SugarLoaf/antigua_sea_qof8pg.webm"
