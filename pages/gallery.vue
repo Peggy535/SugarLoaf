ccc2b99bbb0ad58e7777d38dd11a60a9e84e9b8c
Peggy535 <peggy161152@gmail.com>
2022-09-24 09:47:35 +0100

    Additions to Gallery page

--- pages/gallery.vue
+++ pages/gallery.vue
@@ -143,10 +143,12 @@ onMounted(() => {
 
   items.forEach((el) => {
     const image = el.querySelector("img");
-    el.addEvenetListener("mouseenter", (el) => {
+    el.addEventListener("mouseenter", (el) => {
+      console.log(el);
+      console.log(image);
       gsap.to(".imageOverlay", { duration: 1, autoAlpha: 1 });
     });
-    el.addEvenetListener("mouseleave", (el) => {
+    el.addEventListener("mouseleave", (el) => {
       gsap.to(".imageOverlay", { duration: 1, autoAlpha: 0 });
     });
   });
@@ -155,6 +157,64 @@ onMounted(() => {
 
 <template>
   <div>
+    <section class="h-fit w-screen bg-amber-50">
+      <div class="flex flex-col">
+        <h1 class="text-6xl md:text-8xl xl:text-11xl m-1 sm:m-3 sm:p-3 mb-10">
+          Sugar Loaf
+        </h1>
+        <h1 class="text-4xl md:text-6xl xl:text-8xl m-1 sm:m-3 sm:p-3 mb-5">
+          Photo Gallery
+        </h1>
+        <p
+          class="text-xl md:text-2xl xl:text-3xl sm:w-3/4 xl:w-1/2 m-1 sm:m-3 sm:p-3 w-full mb-3 xl:mb-12 border-l-2 border-l-black"
+        >
+          Discover Sugar Loaf both inside and out. Simply click on the areas you
+          would like to see in more detail or simply hover, click or touch the
+          image you would like to view to see the image and a brief description.
+        </p>
+        <ul
+          class="text-xl sm:text-2xl md:text-3xl xl:text-4xl sm:flex-row mb-3 sm:mb-6 w-3/4 mx-auto text-center bg-brand-yellow bg-opacity-60 rounded-2xl border-4 border-brand-yellow cursor-crosshair"
+        >
+          <div class="my-2 py-2">
+            <li
+              class="m-1 p-1 sm:inline-block transition ease-in-out duration-300 delay-75 hover:text-white pointer-events-auto"
+            >
+              Main Building |
+            </li>
+            <li
+              class="m-1 p-1 sm:inline-block transition ease-in-out duration-300 delay-75 hover:text-white pointer-events-auto"
+            >
+              Pool |
+            </li>
+            <li
+              class="m-1 p-1 sm:inline-block transition ease-in-out duration-300 delay-75 hover:text-white pointer-events-auto"
+            >
+              Balcony area |
+            </li>
+            <li
+              class="m-1 p-1 sm:inline-block transition ease-in-out duration-300 delay-75 hover:text-white pointer-events-auto"
+            >
+              Living Room |
+            </li>
+            <li
+              class="m-1 p-1 sm:inline-block transition ease-in-out duration-300 delay-75 hover:text-white pointer-events-auto"
+            >
+              Kitchen |
+            </li>
+            <li
+              class="m-1 p-1 sm:inline-block transition ease-in-out duration-300 delay-75 hover:text-white pointer-events-auto"
+            >
+              Bedrooms & Bathrooms |
+            </li>
+            <li
+              class="m-1 p-1 sm:inline-block transition ease-in-out duration-300 delay-75 hover:text-white pointer-events-auto"
+            >
+              Swooning Views
+            </li>
+          </div>
+        </ul>
+      </div>
+    </section>
     <main class="h-auto w-screen bg-amber-50 z-10">
       <ul class="flex flex-row flex-wrap">
         <li
@@ -171,7 +231,7 @@ onMounted(() => {
       </ul>
     </main>
     <section
-      class="imageOverlay invisible w-1/2 h-3/4 absolute top-0 z-20 bg-orange-500"
+      class="imageOverlay invisible w-1/2 h-3/4 absolute top-0 z-30 bg-orange-500"
     ></section>
   </div>
 </template>
