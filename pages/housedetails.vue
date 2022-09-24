bc6a2aebde8ea813557225a44e03ed8e68b5aae9
Peggy535 <peggy161152@gmail.com>
2022-09-24 18:11:26 +0100

    Changes to all 3 pages and updates

--- pages/housedetails.vue
+++ pages/housedetails.vue
@@ -1,15 +1,44 @@
 <template>
   <div>
-    <section class="relative h-fit w-screen bg-amber-50">
-      <div class="flex flex-col">
-        <h1 class="text-6xl md:text-8xl xl:text-11xl m-1 sm:m-3 sm:p-3 my-20">
+    <!-- Video section -->
+    <section class="h-screen w-screen fixed z-10">
+      <video
+        class="absolute top-0 right-0 w-screen h-screen object-center object-cover"
+        autoplay
+        loop
+        muted
+        playsinline
+      >
+        <source
+          src="https://res.cloudinary.com/peggy-co/video/upload/q_auto:best/v1664038068/SugarLoaf/production_ID_4782135_1_onpelh.webm"
+          type="video/webm"
+        />
+        <source
+          src="https://res.cloudinary.com/peggy-co/video/upload/q_auto:best/v1664038068/SugarLoaf/production_ID_4782135_1_onpelh.mp4"
+          type="video/mp4"
+        />
+        <source
+          src="https://res.cloudinary.com/peggy-co/video/upload/q_auto:best/v1664038068/SugarLoaf/production_ID_4782135_1_onpelh.mov"
+          type="video/mov"
+        />
+      </video>
+    </section>
+    <section class="relative flex h-screen w-screen z-20">
+      <div
+        class="flex flex-col place-content-end text-slate-800 bg-amber-50 bg-opacity-80 font-NeueMontrealBook"
+      >
+        <h1
+          class="text-7xl md:text-8xl xl:text-10xl m-1 sm:m-3 sm:p-3 mt-20 mb-10"
+        >
           Sugar Loaf
         </h1>
-        <h1 class="text-4xl md:text-6xl xl:text-8xl m-1 sm:m-3 sm:p-3 mb-5">
+        <h1
+          class="text-4xl sm:text-5xl md:text-6xl xl:text-7xl m-1 sm:m-3 sm:p-3 mb-5"
+        >
           House details, amenities and facilities
         </h1>
         <p
-          class="text-xl md:text-3xl xl:text-3xl sm:w-3/4 xl:w-1/2 m-1 p-1 sm:m-3 sm:p-3 w-full mb-3 xl:mb-12 border-l-2 border-l-black"
+          class="text-xl md:text-3xl xl:text-2xl sm:w-3/4 xl:w-1/2 m-1 p-1 sm:m-3 sm:p-3 w-full mb-3 xl:mb-12 border-l-2 border-l-black"
         >
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda non
           dolorem facilis aut possimus voluptatum nostrum nisi. Aspernatur
@@ -24,10 +53,10 @@
           rel="noopener noreferrer"
         >
           <button
-            class="border-2 border-black rounded-2xl transition delay-50 ease-in-out hover:border-brand-yellow duration:300"
+            class="border-2 border-slate-800 rounded-2xl transition delay-50 ease-in-out hover:border-brand-yellow duration:300"
           >
             <p
-              class="text-black p-1 m-1 md:p-2 text-lg md:text-3xl transition delay-50 ease-in-out hover:text-brand-yellow duration:200"
+              class="text-slate-800 p-1 m-1 md:p-2 text-lg md:text-3xl transition delay-50 ease-in-out hover:text-brand-yellow duration:200"
             >
               Book now
             </p>
@@ -35,17 +64,20 @@
         </NuxtLink>
       </span>
     </section>
-    <section
-      class="h-fit w-screen bg-amber-50 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 grid-rows-auto"
-    >
-      <div class="grid col-span-1 row-span-1">
-        <div class="relative flex flex-col basis-full">
+
+    <section class="relative h-fit w-screen bg-amber-50 bg-opacity-80 z-20">
+      <div
+        class="w-full md:w-3/4 items-start flex flex-col mx-auto divide-y divide-slate-600 text-slate-800"
+      >
+        <div
+          class="relative transition duration-200 delay-50 ease-in-out hover:scale-110"
+        >
           <!-- Start of amenities -->
-          <div class="m-1 p-1 border border-black rounded-2xl">
+          <div class="m-1 p-1">
             <div>
-              <h1 class="text-3xl m-1 p-1">General details of Sugar Loaf</h1>
+              <h1 class="text-4xl m-1 p-1">General details of Sugar Loaf</h1>
             </div>
-            <ul class="m-1 p-1">
+            <ul class="m-1 p-1 text-xl list-disc list-inside">
               <li>Up to 8 guests</li>
               <li>4 Bedrooms</li>
               <li>2 Bathrooms | 2 Indoor showers</li>
@@ -56,16 +88,13 @@
             </ul>
           </div>
         </div>
-      </div>
-      <div class="grid col-span-1 row-span-1 ">
-        <div
-          class="relative flex flex-col basis-full"
-        >
-          <div class="m-1 p-1 border border-black rounded-2xl">
+
+        <div class="relative">
+          <div class="m-1 p-1">
             <div>
-              <h1 class="text-3xl m-1 p-1">Scenic Views</h1>
+              <h1 class="text-4xl m-1 p-1">Scenic Views</h1>
             </div>
-            <ul class="m-1 p-1">
+            <ul class="m-1 p-1 text-xl list-disc list-inside">
               <li>
                 Uninterrupted views of Falmouth Harbour from the pool area,
                 outside balcont, living room and bedrooms.
@@ -74,14 +103,13 @@
             </ul>
           </div>
         </div>
-      </div>
-      <div class="grid col-span-1 row-span-1">
-        <div class="relative flex flex-col basis-full">
-          <div class="m-1 p-1 border border-black rounded-2xl">
+
+        <div class="relative">
+          <div class="m-1 p-1">
             <div>
-              <h1 class="text-3xl m-1 p-1">Outdoors</h1>
+              <h1 class="text-4xl m-1 p-1">Outdoors</h1>
             </div>
-            <ul class="m-1 p-1">
+            <ul class="m-1 p-1 list-disc list-inside text-xl">
               <li>
                 Private balcony area extending along two sides of the house
               </li>
@@ -95,15 +123,13 @@
             </ul>
           </div>
         </div>
-      </div>
 
-      <div class="grid col-span-1 row-span-1">
-        <div class="relative flex flex-col basis-full">
-          <div class="m-1 p-1 border border-black rounded-2xl">
+        <div class="relative">
+          <div class="m-1 p-1">
             <div>
-              <h1 class="text-3xl m-1 p-1">Bedrooms & Laundry</h1>
+              <h1 class="text-4xl m-1 p-1">Bedrooms & Laundry</h1>
             </div>
-            <ul class="m-1 p-1">
+            <ul class="m-1 p-1 text-xl list-disc list-inside">
               <li>
                 Housekeeper ('Jolly') for all laundry and washing (Attends 3
                 times per week - this can be extended)
@@ -117,46 +143,39 @@
             </ul>
           </div>
         </div>
-      </div>
 
-      <div class="grid col-span-1 row-span-1">
-        <div class="relative flex flex-col basis-full">
-          <div class="m-1 p-1 border border-black rounded-2xl">
+        <div class="relative">
+          <div class="m-1 p-1">
             <div>
-              <h1 class="text-3xl m-1 p-1">Heating & cooling</h1>
+              <h1 class="text-4xl m-1 p-1">Heating & cooling</h1>
             </div>
-            <ul class="m-1 p-1">
+            <ul class="m-1 p-1 text-xl list-disc list-inside">
               <li>Air Conditioning</li>
               <li>Ceiling fans</li>
               <li>Portable fans</li>
-              <li class=""></li>
             </ul>
           </div>
         </div>
-      </div>
 
-      <div class="grid col-span-1 row-span-1">
-        <div class="relative flex flex-col basis-full">
-          <div class="m-1 p-1 border border-black rounded-2xl">
+        <div class="relative">
+          <div class="m-1 p-1">
             <div>
-              <h1 class="text-3xl m-1 p-1">Entertainment</h1>
+              <h1 class="text-4xl m-1 p-1 pr-10">Entertainment</h1>
             </div>
-            <ul class="m-1 p-1">
+            <ul class="m-1 p-1 text-xl list-disc list-inside">
               <li>Wifi</li>
               <li>TV in main living room</li>
               <li>Anything else??</li>
             </ul>
           </div>
         </div>
-      </div>
 
-      <div class="grid col-span-1 row-span-1">
-        <div class="relative flex flex-col basis-full">
-          <div class="m-1 p-1 border border-black rounded-2xl">
+        <div class="relative">
+          <div class="m-1 p-1">
             <div>
-              <h1 class="text-3xl m-1 p-1">Bathrooms</h1>
+              <h1 class="text-4xl m-1 p-1">Bathrooms</h1>
             </div>
-            <ul class="m-1 p-1">
+            <ul class="m-1 p-1 text-xl list-disc list-inside">
               <li>Hot Water</li>
               <li>Hair dryers</li>
               <li>2 indoor showers | 1 outdoor shower</li>
@@ -164,15 +183,13 @@
             </ul>
           </div>
         </div>
-      </div>
 
-      <div class="grid col-span-1 row-span-1">
-        <div class="relative flex flex-col basis-full">
-          <div class="border border-black rounded-2xl">
+        <div class="relative">
+          <div class="m-1 p-1">
             <div>
-              <h1 class="text-3xl m-1 p-1">Wheelchair access</h1>
+              <h1 class="text-4xl m-1 p-1">Wheelchair access</h1>
             </div>
-            <ul class="m-1 p-1">
+            <ul class="m-1 p-1 text-xl list-disc list-inside">
               <li>
                 Wheelchairs have access to the main villa level and the pool
                 level.
@@ -181,15 +198,13 @@
             </ul>
           </div>
         </div>
-      </div>
 
-      <div class="grid col-span-1 row-span-1">
-        <div class="relative flex flex-col basis-full">
-          <div class="m-1 p-1 border border-black rounded-2xl">
+        <div class="relative">
+          <div class="m-1 p-1">
             <div>
-              <h1 class="text-3xl m-1 p-1">Other important aspects</h1>
+              <h1 class="text-4xl m-1 p-1">Other important aspects</h1>
             </div>
-            <ul class="m-1 p-1">
+            <ul class="m-1 p-1 text-xl list-disc list-inside">
               <li>
                 At the House, the back-up generator will provide a rapid pick up
                 in the event of occasional power drops, and there is a TV to
@@ -207,7 +222,6 @@
           </div>
         </div>
       </div>
-
     </section>
   </div>
 </template>
