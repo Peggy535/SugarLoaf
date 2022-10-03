660776d94d07e619549db5e4642cefbcdbbc94bf
Peggy535 <peggy161152@gmail.com>
2022-10-03 16:02:33 +0100

    pwa dets updated

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -30,7 +30,7 @@ export default defineNuxtConfig({
         "A luxurious Antiguan residence that is Home from Home. Set in large and quiet gardens overlooking Falmouth Harbour, Sugar Loaf has views to swoon at and plenty of privacy, peace and quiet",
       ogTitle: "Sugar Loaf - An Antiguan Residence",
       ogImage: {
-        path: "icon.png",
+        path: "/icon.png",
         width: 512,
         height: 512,
       },
