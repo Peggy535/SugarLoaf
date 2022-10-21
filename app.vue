62d23dce05aba4ab0e42c801eccf3fd07366d710
Peggy535 <peggy161152@gmail.com>
2022-10-21 18:33:05 +0100

    no script gtm added

--- app.vue
+++ app.vue
@@ -10,6 +10,23 @@
 	const favicon = ref("/icon.png");
 
 	useHead({
+		script: [
+			{
+				hid: "gtm",
+				children: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+							new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+							j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+							'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+							})(window,document,'script','dataLayer','GTM-WGK3R56');`,
+				type: "text/javascript",
+			},
+		],
+		noscript: [
+			{
+				children: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WGK3R56"
+							height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+			},
+		],
 		meta: [
 			{
 				name: "locale",
